abd1011195564e315ca8204a65828f67559cc88d
Maycol Uribe Romero <maycolur7@gmail.com>
2018-07-03 16:42:38 +0200

    ul for the realated menu added

--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -6,6 +6,9 @@ function Related(props) {
     return(
         <div className="related-container">
             <img src={ logo }  width="250"/>
+            <ul>
+
+            </ul>
         </div>
         
     )
