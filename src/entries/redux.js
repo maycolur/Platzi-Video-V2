d5240707601c197690e40226c5cfbde16eb360c0
Maycol Uribe Romero <maycolur7@gmail.com>
2018-07-18 09:54:24 +0200

    Redux Test

---
+++ src/entries/redux.js
@@ -0,0 +1,13 @@
+
+// Redux Test
+
+const $form = document.getElementById('form')
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const data = new FormData($form)
+    const title = data.get('title')
+    console.log(title)
+    
+}
