09809a68a28749ef8746e4e1025cdcce443c34ec
Maycol Uribe Romero <maycolur7@gmail.com>
2019-02-08 17:17:29 +0100

    implementation in reducer of get videos of all categories now placed in a function

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -3,20 +3,14 @@ import Category from "../categories/components/category";
 /*
 *  Reducer to manage the api
 */
-
 function data (state, action) {
   console.log(state)
   let results = []
   switch (action.type) {
     case 'SEARCH_VIDEO': {
       if (action.payload.query) {
-        let videos = []
-        for(let category of state.data.categories) {
-          for(let i=0; i<category.playlist.length; i++) {
-            videos.push(category.playlist[i])
-          }
-        }
-        results = videos.filter(item => item.title.includes(action.payload.query))
+        let videoList = getVideosOfAllCategories(state.data.categories)
+        results = videoList.filter(item => item.title.includes(action.payload.query))
         console.log(results)
       }
      return {
@@ -27,6 +21,16 @@ function data (state, action) {
     default:
       return state
   }
+}
+
+function getVideosOfAllCategories(categories) {
+  let videos = []
+  for(let category of categories) {
+    for(let i=0; i<category.playlist.length; i++) {
+      videos.push(category.playlist[i])
+    }
+  }
+  return videos
 }
 
 export default data
