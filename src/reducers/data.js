741e015fdf76d359d7b397ddef10be91c8955cc6
Maycol Uribe Romero <maycolur7@gmail.com>
2019-02-08 10:23:46 +0100

    data reducer check for matches  in all playlist

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,16 +1,23 @@
+import Category from "../categories/components/category";
+
 /*
 *  Reducer to manage the api
 */
 
 function data (state, action) {
+  console.log(state)
+  let results = []
   switch (action.type) {
     case 'SEARCH_VIDEO': {
-      let results = []
       if (action.payload.query) {
-        const list = state.data.categories[2].playlist
-        results = list.filter((item) => {
-          return item.author.includes(action.payload.query)
-        })
+        let videos = []
+        for(let category of state.data.categories) {
+          for(let i=0; i<category.playlist.length; i++) {
+            videos.push(category.playlist[i])
+          }
+        }
+        results = videos.filter(item => item.title.includes(action.payload.query))
+        console.log(results)
       }
      return {
        ...state,
